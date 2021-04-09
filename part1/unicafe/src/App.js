e7e1fa6e26f45289a3462347f335ae0743942a29
Dave Yoon <daveyoon64@gmail.com>
2021-04-08 21:03:07 -0700

    solution for 1.6

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,10 +6,37 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleClick = (name) => {
+    switch (name) {
+      case 'good': return () => setGood(good + 1)
+      case 'neutral': return () => setNeutral(neutral + 1)
+      case 'bad': return () => setBad(bad + 1)
+      default: return
+    }
+  }
+
   return (
-    <div>
-      code here
+    <div id="App">
+      <div id="feedback-form">
+      <h1>give feedback</h1>
+      <Button name="good" linkHandler={handleClick} />
+      <Button name="neutral" linkHandler={handleClick} />
+      <Button name="bad" linkHandler={handleClick} />
+      </div>
+      <div id="stat-list">
+        <h1>statistics</h1>
+        <div>good {good}</div>
+        <div>neutral {neutral}</div>
+        <div>bad {bad}</div>
+      </div>
+      
     </div>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <button onClick={props.linkHandler(props.name)}>{props.name}</button>
   )
 }
 
