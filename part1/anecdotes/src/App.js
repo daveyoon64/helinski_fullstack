91741708132b5abefe9cd31351ba7281ee38d8b5
Dave Yoon <daveyoon64@gmail.com>
2021-04-15 11:55:11 -0700

    completed ex 1.14

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const App = () => {
   const anecdotes = [
@@ -12,6 +12,7 @@ const App = () => {
    
   const [selected, setSelected] = useState(0);
   const [votes, setVotes] = useState({0:0, 1:0, 2:0, 3:0, 4:0, 5:0});
+  const [largest, setLargest] = useState(0);
 
   const handleClick = () => {
     let number = Math.floor(Math.random() * (anecdotes.length - 1));
@@ -24,21 +25,31 @@ const App = () => {
     setVotes(votes_copy);
   }
 
+  useEffect(() => {
+    if (votes[selected] > votes[largest]) {
+      setLargest(selected)
+    }
+  }, [votes, largest, selected])
+
   return (
     <div>
+      <h1>Anecdote of the day</h1>
       {anecdotes[selected]}
       <div>has {votes[selected]} votes</div>
       <Button name="vote"
         linkHandler={handleVote} />
       <Button name="next anecdote" 
         linkHandler={handleClick} />
+      <h1>Anecdote with most votes</h1>
+      {anecdotes[largest]}
+      <div>has {votes[largest]} votes</div>
     </div>
   )
 }
 
-const Button = (props) => {
+const Button = ({linkHandler, name}) => {
   return (
-    <button onClick={props.linkHandler}>{props.name}</button>
+    <button onClick={linkHandler}>{name}</button>
   )
 } 
 
