37fa335cbc7cc94e0c29fe3488c49ad5ba1116c9
Dave Yoon <daveyoon64@gmail.com>
2021-04-19 14:15:44 -0700

    completed ex 214

--- part2/countries/src/components/Country.js
+++ part2/countries/src/components/Country.js
@@ -1,7 +1,9 @@
 import {v4 as uuidv4} from 'uuid'
+import Weather from './Weather'
 
 const Country = ({country}) => {
   const width = {width: '10vw'}
+
   return (
     <div>
       <h1>{country.name}</h1>
@@ -17,8 +19,12 @@ const Country = ({country}) => {
       <div className="flag">
         <img src={country.flag} alt='' style={width} />
       </div>
+      <div>
+        <Weather country={country}/>
+      </div>
     </div>
   )
 }
 
 export default Country;
+
---
+++ part2/countries/src/components/Weather.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react'
+
+import axios from 'axios'
+
+const Weather = ({country}) => {
+  const [weather, setWeather] = useState({
+    current: {
+      temperature: '0',
+      weather_icons: [],
+      wind_speed: 0,
+      wind_dir: 'N'
+    }
+  })
+
+  useEffect(() => {
+    axios
+      .get(`http://api.weatherstack.com/current?access_key=${process.env.REACT_APP_WEATHER_API_CODE}&query=${country.capital}`)
+      .then(response => {
+        setWeather(response.data)
+      })
+      .catch(error => {
+        console.log('error', error)
+      })
+  }, [country.capital])
+
+  return (
+    <div>
+      <h2>Weather in {country.capital}</h2>
+      <div><b>temperature:</b> {weather.current.temperature} Celcius</div>
+      <img src={weather.current.weather_icons[0]} alt=''/>
+      <div><b>wind:</b> {weather.current.wind_speed} mph direction {weather.current.wind_dir}</div>
+    </div>
+  )
+}
+
+export default Weather
