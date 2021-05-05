bcfe3bc39b1e593d0b7d82375e9ffcf5d7cf3dcc
Dave Yoon <daveyoon64@gmail.com>
2021-05-04 19:03:25 -0700

    alternate way of writing the phonebook service

--- part2/phonebook/src/services/phonebook.js
+++ part2/phonebook/src/services/phonebook.js
@@ -3,8 +3,9 @@ import axios from 'axios';
 const baseUrl = 'http://localhost:3001/persons'
 
 const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
+  // const request = axios.get(baseUrl)
+  // return request.then(response => response.data)
+  return axios.get(baseUrl).then(response => response.data)
 }
 
 const addPerson = (obj) => {
